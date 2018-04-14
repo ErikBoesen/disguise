8c206758be120299292f20fe2de492943551c5f0
ErikBoesen <me@erikboesen.com>
2018-04-14 14:04:20 -0400

    Add initial functional script

---
+++ abrir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+app_root="$1"
+new_name="$2"
+info="$app_root/Contents/Info.plist"
+macos="$app_root/Contents/MacOS"
+binary="$(ls $macos)"
+
+plutil -replace CFBundleExecutable -string "$new_name" "$info"
+mv "$macos/$binary" "$macos/$new_name"
+mv "$app_root" "$(dirname "$app_root")/$new_name.app"
