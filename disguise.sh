fba3fee5ec917ff82501b1917b3c8b76f7f53a9c
ErikBoesen <me@erikboesen.com>
2018-09-06 18:04:03 -0400

    Stop requiring users to add .app to the end of new name

--- disguise.sh
+++ disguise.sh
@@ -16,24 +16,24 @@ if [[ $# -lt 1 ]]; then
 fi
 
 app_root="$1"
-new_name="$2"
-if [[ -z $new_name ]]; then
-    new_name=$(openssl rand -hex 8).app
+new_name="${2%.app}"
+if [[ -z ${new_name}.app ]]; then
+    new_name=$(openssl rand -hex 8)
 fi
 
 info="$app_root/Contents/Info.plist"
 macos="$app_root/Contents/MacOS"
+# TODO: plutil has replaced PlistBuddy, but cannot seem to read properties as we here desire.
 binary="$(/usr/libexec/PlistBuddy "$info" -c "Print :CFBundleExecutable")"
-new_binary=${new_name%.app}
 
 task "Overwriting CFBundleName property"
-plutil -replace CFBundleName -string "$new_binary" "$info"
+plutil -replace CFBundleName -string "$new_name" "$info"
 task "Renaming executable"
-mv "$macos/$binary" "$macos/$new_binary"
+mv "$macos/$binary" "$macos/$new_name"
 task "Renaming bundle"
-mv "$app_root" "$(dirname "$app_root")/$new_name"
+mv "$app_root" "$(dirname "$app_root")/$new_name.app"
 task "Creating symlink"
-cd "$new_name/Contents/MacOS"
-ln -s "$new_binary" "$binary"
+cd "$new_name.app/Contents/MacOS"
+ln -s "$new_name" "$binary"
 
-succ "Successfully disguised $app_root as $new_name."
+succ "Successfully disguised $app_root as $new_name.app."
